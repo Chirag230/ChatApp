5bcf0a3470f3d76888ba79367c94ff7aaf66bbe5
Chirag230 <chirag2306garg@gmail.com>
2024-06-15 14:26:44 +0530

    url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000"
+			const socket = io("https://chatapp-8mwr.onrender.com/"
 				, 
 				{
 				query: {
